4fbdc204bbf2e6e30f35fd04dc54a39a836ed8d5
AstroYoung617 <413294996@qq.com>
2021-04-29 18:26:41 +0800

    改进代码

--- Chapter2/resource/2_18.cpp
+++ Chapter2/resource/2_18.cpp
@@ -3,12 +3,11 @@
 int main() {
 	int val = 5;
 	int* p = &val;
-	int vall = 10;
 	std::cout << p << std::endl;
-	std::cout << *p << std::endl;
-	p = &vall;//将指针p指向对象val的地址
+	std::cout << *p << std::endl;//解引用操作仅适用于那些确定指向了某个对象的指针
+	*p = 6;
 	std::cout << p << std::endl;
-	std::cout << *p << std::endl;
+	std::cout << val << std::endl;
 
 	return 0;
 }
